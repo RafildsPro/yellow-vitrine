7e973418bc21739eae28da828e46a0909c6044d3
RafildsPro <rafaelgsaab@gmail.com>
2026-05-31 00:15:03 -0300

    OAuth proxy proprio no Vercel

---
+++ api/callback.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+  const response = await fetch('https://github.com/login/oauth/access_token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+    body: JSON.stringify({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code
+    })
+  });
+  const data = await response.json();
+  const token = data.access_token;
+  const content = JSON.stringify({ token, provider: 'github' });
+  res.send(`<script>
+    (function() {
+      function receiveMessage(e) {
+        window.opener.postMessage('authorization:github:success:' + '${content}', e.origin);
+      }
+      window.addEventListener("message", receiveMessage, false);
+      window.opener.postMessage("authorizing:github", "*");
+    })();
+  </script>`);
+}
